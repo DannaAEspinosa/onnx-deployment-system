331f3dd65f48fdba255e50dcf8fbde49c02c6840
DannaAEspinosa <danna.espinosa1@u.icesi.edu.co>
2025-06-09 18:28:37 -0500

    fix:tests

--- tests/test_mnist_model.py
+++ tests/test_mnist_model.py
@@ -1,40 +1,39 @@
-import unittest
-import onnxruntime as ort
+from PIL import Image
 import numpy as np
+import onnxruntime as ort
+import unittest
 from tensorflow.keras.datasets import mnist
+import io
+
+def preprocess(img_array):
+    img = Image.fromarray(img_array).convert('L').resize((28, 28))
+    img_arr = np.array(img).astype(np.float32)
+    img_arr = 255 - img_arr  # Invertir colores si el fondo es blanco
+    img_arr /= 255.0
+    img_arr = img_arr.reshape(1, 1, 28, 28)
+    return img_arr
 
 class TestMNISTModel(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        # Cargar modelo ONNX
         cls.session = ort.InferenceSession("model.onnx")
-        cls.input_name = cls.session.get_inputs()[0].name
+        (cls.x_test, cls.y_test), _ = mnist.load_data()
 
-        # Cargar dataset de prueba (solo 1 imagen)
-        (_, _), (cls.x_test, cls.y_test) = mnist.load_data()
-        cls.x_test = cls.x_test.astype(np.float32)
-        cls.x_test = (255 - cls.x_test) / 255.0  # Inversión de color + normalización
-        cls.x_test = cls.x_test.reshape(-1, 1, 28, 28)
-
-    def test_prediction_output(self):
-        input_tensor = self.x_test[0:1]
-        pred = self.session.run(None, {self.input_name: input_tensor})
-        pred_class = int(np.argmax(pred[0]))
-
-        self.assertIsInstance(pred_class, int)
+    def test_model_outputs(self):
+        # Simple prueba con una imagen
+        input_tensor = preprocess(self.x_test[0])
+        input_name = self.session.get_inputs()[0].name
+        output = self.session.run(None, {input_name: input_tensor})
+        predicted = np.argmax(output[0])
+        self.assertIsInstance(predicted, int)
 
     def test_prediction_accuracy_threshold(self):
-        # Probar que al menos 60% de las primeras 10 predicciones son correctas
+        input_name = self.session.get_inputs()[0].name
         correct = 0
         for i in range(10):
-            input_tensor = self.x_test[i:i+1]
-            pred = self.session.run(None, {self.input_name: input_tensor})
-            pred_class = int(np.argmax(pred[0]))
-            if pred_class == int(self.y_test[i]):
+            img = preprocess(self.x_test[i])
+            pred = np.argmax(self.session.run(None, {input_name: img})[0])
+            if pred == self.y_test[i]:
                 correct += 1
-
-        self.assertGreaterEqual(correct, 6)  # Al menos 60% (6/10)
-
-if __name__ == '__main__':
-    unittest.main()
+        print(f"Correct predictions: {correct}/10")
+        self.assertGreaterEqual(correct, 6)  # 60%
